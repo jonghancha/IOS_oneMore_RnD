1df5d07e7627b9c6b9b7ec86f57a3c7aa2b484b9
jonghancha <jonghancha@gmail.com>
2021-02-21 01:59:00 +0900

    02-21

---
+++ Tabata 2/Tabata/TabataTimerSetViewController.swift
@@ -0,0 +1,165 @@
+//
+//  TabataTimerSetViewController.swift
+//  Tabata
+//
+//  Created by TJ on 2021/02/19.
+//
+
+import UIKit
+import SimpleAlertPickers
+
+class TabataTimerSetViewController: UIViewController {
+    
+    @IBOutlet weak var tabataUIView: UIView!
+    @IBOutlet weak var tabataStartButton: UIButton!
+    
+    @IBOutlet weak var tabataRoundButton: UIButton!
+    @IBOutlet weak var tabataWorkButton: UIButton!
+    @IBOutlet weak var tabataRestButton: UIButton!
+    
+    
+    
+    @IBOutlet weak var tabataRepButton: UIButton!
+    
+    @IBOutlet weak var tabataRepSetLabel: UILabel!
+    @IBOutlet weak var tabataRepRestLabel: UILabel!
+    @IBOutlet weak var tabataRepSetButton: UIButton!
+    @IBOutlet weak var tabataRepRestButton: UIButton!
+    
+    /////////////////////////////////////////////////////
+    //
+    // 버튼을 누르면 Alert형태로 팝업되는 picker입니다.
+    // picker에서 선택한후 완료를 누르면 버튼에 선택한 값이 표시됩니다.
+    //
+    //
+    /////////////////////////////////////////////////////
+    
+    // 피커에 들어갈 배열 구성.
+    let times: [String] = (1...10).map { String($0) }
+    // 피커에서 선택된 값 변수
+    var pickedValue: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tabataUIView.layer.masksToBounds = true
+        tabataUIView.layer.cornerRadius = 20
+        
+        tabataStartButton.layer.masksToBounds = true
+        tabataStartButton.layer.cornerRadius = 10
+        
+        tabataRoundButton.layer.masksToBounds = true
+        tabataRoundButton.layer.cornerRadius = 10
+        tabataWorkButton.layer.masksToBounds = true
+        tabataWorkButton.layer.cornerRadius = 10
+        tabataRestButton.layer.masksToBounds = true
+        tabataRestButton.layer.cornerRadius = 10
+        
+        tabataRepSetButton.layer.masksToBounds = true
+        tabataRepSetButton.layer.cornerRadius = 10
+        tabataRepRestButton.layer.masksToBounds = true
+        tabataRepRestButton.layer.cornerRadius = 10
+        
+        var second = 0
+        var minute = 0
+        
+        for i in 1...20{
+              if i*15 % 60 == 0{
+                minute += 1
+                second = 0
+              }else{
+                second += 15
+              }
+              if second == 0{
+                print("0\(minute):0\(second)")
+              }else{
+                print("0\(minute):\(second)")
+              }
+            }
+        
+        
+            for i in 1...20{
+              if i*15 % 60 == 0{
+                minute += 1
+                second = 0
+              }else{
+                second += 15
+              }
+              if second == 0{
+                print("0\(minute):0\(second)")
+              }else{
+                print("0\(minute):\(second)")
+              }
+            }
+        
+        for i in 1...20{
+              if i*15 % 60 == 0{
+                minute += 1
+                second = 0
+              }else{
+                second += 15
+              }
+              if second == 0{
+                print("0\(minute):0\(second) 분")
+              }else{
+                print("0\(minute):\(second) 분")
+              }
+            }
+            for i in 6...100{
+              if i<10{
+                print("0\(i):00 분")
+              }else{
+                print("\(i):00 분")
+              }
+            }
+        
+        pickedValue = times[0] // <-- 뷰가 생성될때 피커와 버튼에 값 주기
+        //btnShowTime.setTitle = times[0]
+    }
+    
+    // 버튼 클릭시 액션
+    @IBAction func btnTime(_ sender: UIButton) {
+        
+        // Alert 형식
+        let alert = UIAlertController( title: "시간을 정해주세요", message: nil, preferredStyle: UIAlertController.Style.alert)
+        
+        // 1~10까지를 String 타입 배열로 구성
+//        let times: [String] = (1...10).map { String($0) }
+        
+        // addPickerView에 매개변수로 있는 values 자체가 [[String]] 형식이라 맞춰서 써야 될거같습니다.
+        let pickerViewValues: [[String]] = [times]
+        let pickerViewSelectedValue: PickerViewViewController.Index = (column: 0, row: times.firstIndex(of: pickedValue!) ?? 0) // picker가 생성될 때 pickedValue가 선택된 상태로 alert가 뜸.
+
+        // alert에 picker 추가해주기 + async를 통해 버튼의 text 바꿔줌.
+        // picker 선택시 action은 이 안에 써주시면 됩니다.
+        alert.addPickerView(values: pickerViewValues, initialSelection: pickerViewSelectedValue) { vc, picker, index, values in
+            DispatchQueue.main.async {
+                UIView.animate(withDuration: 1) {
+                    self.pickedValue = self.times[index.row] // 피커에서 선택한 값 변수에 저장해주기.
+                    self.btnShowTime.setTitle(self.pickedValue, for: .normal)
+                }
+            }
+        }
+        
+        
+        alert.addAction(title: "완료", style: .cancel)
+        alert.show()
+        
+    }
+    
+    @IBAction func tabataRepButton(_ sender: UIButton) {
+        tabataRepSetLabel.isHidden = true
+        print("pushed repButton")
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- pickerRnD/pickerRnD/ViewController.swift
+++ pickerRnD/pickerRnD/ViewController.swift
@@ -18,6 +18,11 @@ class ViewController: UIViewController {
     //
     /////////////////////////////////////////////////////
     
+    // 피커에 들어갈 배열 구성.
+    let times: [String] = (1...10).map { String($0) }
+    // 피커에서 선택된 값 변수
+    var pickedValue: String?
+    
     
     // 버튼에 텍스트 띄우기 위해 잡아줌
     @IBOutlet weak var btnShowTime: UIButton!
@@ -26,6 +31,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+        pickedValue = times[0] // <-- 뷰가 생성될때 피커와 버튼에 값 주기
+        //btnShowTime.setTitle = times[0]
         
     }
 
@@ -38,18 +45,19 @@ class ViewController: UIViewController {
         let alert = UIAlertController( title: "시간을 정해주세요", message: nil, preferredStyle: UIAlertController.Style.alert)
         
         // 1~10까지를 String 타입 배열로 구성
-        let times: [String] = (1...10).map { String($0) }
+//        let times: [String] = (1...10).map { String($0) }
         
         // addPickerView에 매개변수로 있는 values 자체가 [[String]] 형식이라 맞춰서 써야 될거같습니다.
         let pickerViewValues: [[String]] = [times]
-        let pickerViewSelectedValue: PickerViewViewController.Index = (column: 0, row: times.firstIndex(of: "1") ?? 0)
+        let pickerViewSelectedValue: PickerViewViewController.Index = (column: 0, row: times.firstIndex(of: pickedValue!) ?? 0) // picker가 생성될 때 pickedValue가 선택된 상태로 alert가 뜸.
 
         // alert에 picker 추가해주기 + async를 통해 버튼의 text 바꿔줌.
         // picker 선택시 action은 이 안에 써주시면 됩니다.
         alert.addPickerView(values: pickerViewValues, initialSelection: pickerViewSelectedValue) { vc, picker, index, values in
             DispatchQueue.main.async {
                 UIView.animate(withDuration: 1) {
-                    self.btnShowTime.setTitle(times[index.row], for: .normal)
+                    self.pickedValue = self.times[index.row] // 피커에서 선택한 값 변수에 저장해주기. 
+                    self.btnShowTime.setTitle(self.pickedValue, for: .normal)
                 }
             }
         }
